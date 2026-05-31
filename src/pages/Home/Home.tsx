fcfb3a3189577176a669f9fbea63677fb8bf12dc
qayumjon-coder <atajanovqayumjonbek@gmail.com>
2026-06-01 00:18:27 +0500

    News card and some packages installed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import Header from "../../components/Header"
+import NewsCard from "../../components/NewsCard"
 
 const Home: React.FC = () => {
   return (
     <>
         <Header />
-        <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-          <p className="text-lg text-gray-600">This is the home page of our React application.</p>
+        <div className="flex flex-col items-center mt-8 h-screen">
+
+          <h1 className="text-4xl font-bold mb-4">Latest News</h1>
+          
+          <NewsCard />
         </div>
     </>
   )
