7d74f89b7f397a39430eda7ddf01c5ffdba653fa
qayumjon-coder <atajanovqayumjonbek@gmail.com>
2026-06-23 18:04:20 +0500

    New font and smooth scroll added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -41,7 +41,7 @@ const Home: React.FC = () => {
 
           <div className="flex w-full justify-center items-center">
             <div className="absolute  mb-4 z-2 top-[50%] font-satisfy">
-              <p className="font-satisfy tracking-wider text-center">Ko'chada 50 daraja issiq 🥵</p>
+              <p className="font-satisfy tracking-wider text-center text-lg -mt-4 mb-4">Blogdagi so'ngi yangiliklar</p>
               <h1 className="text-8xl text-blue-700 font-bold -rotate-5">Welcome to HomePage</h1>
             </div>
             <span className="absolute w-180 h-180 bg-white rounded-full  blur-xl top-0"></span>
@@ -52,9 +52,9 @@ const Home: React.FC = () => {
 
       <section className="w-full flex justify-center relative">
         <span className="absolute w-[110%] h-25 bg-white blur-md -left-5 -top-5"></span>
-        <div className="w-360 flex flex-col items-center pt-40 h-screen gap-10">
+        <div className="w-360 flex flex-col items-center pt-30 h-screen gap-10">
 
-          <h2 className="text-5xl text-left w-full">Latest news</h2>
+          <h2 className="text-5xl text-center w-full font-eater font-bold tracking-[30px] rotate-x-80 hover:rotate-x-0 transition text-shadow-md text-shadow-black/50">Latest news</h2>
 
           <div className="w-full flex justify-between">
             {newsCard.map((item) => (
